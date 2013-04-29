fe68d50d543c5b9c1053d8db86d4e82b66f680db
Nick <nick@mint14.(none)>
2013-04-28 20:03:23 -0500

    updated to allow for possible effects, glissando implemented as a sample

--- vc_music/vc_music.ino
+++ vc_music/vc_music.ino
@@ -9,236 +9,257 @@
 // Pins 6,7 -> piezo speaker
 
 #include <avr/pgmspace.h>
+
 #include "pitches.h"
 
 #define PERIOD        20000
 #define BEAT          61440
-#define DELAY_MILLIS  16
+#define DELAY_MICROS  16
 
-// uncomment for pin 13 LED diagnostic
-//#define DIAG
+//#define USE_EFX
 
-const int score[][3] PROGMEM = {
-  {       0, NOTE_E2, 16 },
-  { NOTE_E4, NOTE_E2, 16 },
-  { NOTE_A4, NOTE_A3, 16 },
-  { NOTE_C5, NOTE_A3, 16 },
-  { NOTE_B4, NOTE_A3, 16 },
-  { NOTE_E4, NOTE_A3, 16 },
-  { NOTE_B4, NOTE_GS3, 16 },
-  { NOTE_D5, NOTE_GS3, 16 },
-  { NOTE_C5, NOTE_A3, 16 },
-  { NOTE_C5, NOTE_E3, 16 },
-  { NOTE_E5, NOTE_A3, 16 },
-  { NOTE_E5, NOTE_C4, 16 },
-  { NOTE_GS4, NOTE_B3, 16 },
-  { NOTE_GS4, NOTE_E3, 16 },
-  { NOTE_E5, NOTE_B3, 16 },
-  { NOTE_E5, NOTE_D4, 16 },
-  { NOTE_A4, NOTE_C4, 16 },
-  { NOTE_E4, NOTE_C4, 16 },
-  { NOTE_A4, NOTE_A3, 16 },
-  { NOTE_C5, NOTE_A3, 16 },
-  { NOTE_B4, NOTE_GS3, 16 },
-  { NOTE_E4, NOTE_GS3, 16 },
-  { NOTE_B4, NOTE_E3, 16 },
-  { NOTE_D5, NOTE_E3, 16 },
-  { NOTE_C5, NOTE_A3, 16 },
-  { NOTE_C5, NOTE_E3, 16 },
-  { NOTE_A4, NOTE_A3, 16 },
-  { NOTE_A4, NOTE_C4, 16 },
-  {       0, NOTE_B3, 16 },
-  {       0, NOTE_E3, 16 },
-  {       0, NOTE_B3, 16 },
-  {       0, NOTE_D4, 16 },
-  {       0, NOTE_C4, 16 },
-  { NOTE_E5, NOTE_C4, 16 },
-  { NOTE_C5, NOTE_A3, 16 },
-  { NOTE_E5, NOTE_A3, 16 },
-  { NOTE_A4, NOTE_C4, 16 },
-  { NOTE_C5, NOTE_C4, 16 },
-  { NOTE_E4, NOTE_A3, 16 },
-  { NOTE_G4, NOTE_A3, 16 },
-  { NOTE_F4, NOTE_D4, 16 },
-  { NOTE_F4, NOTE_A3, 16 },
-  { NOTE_A4, NOTE_F3, 16 },
-  { NOTE_A4, NOTE_A3, 16 },
-  { NOTE_D5, NOTE_D3, 16 },
-  { NOTE_D5, NOTE_F3, 16 },
-  { NOTE_F5, NOTE_E2, 16 },
-  { NOTE_F5, NOTE_C3, 16 },
-  { NOTE_F5, NOTE_B2, 16 },
-  { NOTE_D5, NOTE_B2, 16 },
-  { NOTE_B4, NOTE_D3, 16 },
-  { NOTE_D5, NOTE_D3, 16 },
-  { NOTE_G4, NOTE_E3, 16 },
-  { NOTE_B4, NOTE_E3, 16 },
-  { NOTE_D4, NOTE_B3, 16 },
-  { NOTE_F4, NOTE_B3, 16 },
-  { NOTE_E4, NOTE_B3, 16 },
-  { NOTE_E4, NOTE_E3, 16 },
-  { NOTE_G4, NOTE_C3, 16 },
-  { NOTE_G4, NOTE_E3, 16 },
-  { NOTE_C5, NOTE_A2, 16 },
-  { NOTE_C5, NOTE_C3, 16 },
-  { NOTE_E5, NOTE_G2, 16 },
-  { NOTE_E5, NOTE_B2, 16 },
-  { NOTE_E5, NOTE_A2, 16 },
-  { NOTE_C5, NOTE_A2, 16 },
-  { NOTE_A4, NOTE_C3, 16 },
-  { NOTE_C5, NOTE_C3, 16 },
-  { NOTE_F4, NOTE_D3, 16 },
-  { NOTE_F4, NOTE_F3, 16 },
-  { NOTE_D5, NOTE_B2, 16 },
-  { NOTE_D5, NOTE_D3, 16 },
-  { NOTE_D5, NOTE_G2, 16 },
-  { NOTE_B4, NOTE_G2, 16 },
-  { NOTE_G4, NOTE_B2, 16 },
-  { NOTE_B4, NOTE_B2, 16 },
-  { NOTE_E4, NOTE_C3, 16 },
-  { NOTE_E4, NOTE_E3, 16 },
-  { NOTE_C5, NOTE_A2, 16 },
-  { NOTE_C5, NOTE_C3, 16 },
-  { NOTE_C5, NOTE_F2, 16 },
-  { NOTE_A4, NOTE_F2, 16 },
-  { NOTE_F4, NOTE_G2, 16 },
-  { NOTE_A4, NOTE_G2, 16 },
-  { NOTE_D4, NOTE_G2, 16 },
-  { NOTE_D4, NOTE_G3, 16 },
-  { NOTE_B4, NOTE_F3, 16 },
-  { NOTE_B4, NOTE_G3, 16 },
-  { NOTE_C5, NOTE_C3, 16 },
-  { NOTE_C5, NOTE_G3, 16 },
-  {       0, NOTE_C4, 16 },
-  {       0, NOTE_E4, 16 },
-  {       0, NOTE_D4, 16 },
-  {       0, NOTE_G3, 16 },
-  {       0, NOTE_D4, 16 },
-  {       0, NOTE_F4, 16 },
-  {       0, NOTE_E4, 16 },
-  { NOTE_G4, NOTE_E4, 16 },
-  { NOTE_C5, NOTE_C4, 16 },
-  { NOTE_E5, NOTE_C4, 16 },
-  { NOTE_D5, NOTE_B3, 16 },
-  { NOTE_G4, NOTE_B3, 16 },
-  { NOTE_D5, NOTE_G3, 16 },
-  { NOTE_F5, NOTE_G3, 16 },
-  { NOTE_E5, NOTE_C4, 16 },
-  { NOTE_E5, NOTE_G3, 16 },
-  { NOTE_G5, NOTE_C4, 16 },
-  { NOTE_G5, NOTE_E4, 16 },
-  { NOTE_B4, NOTE_D4, 16 },
-  { NOTE_B4, NOTE_G3, 16 },
-  { NOTE_G5, NOTE_D4, 16 },
-  { NOTE_G5, NOTE_F4, 16 },
-  { NOTE_C5, NOTE_E4, 16 },
-  { NOTE_G4, NOTE_E4, 16 },
-  { NOTE_C5, NOTE_C4, 16 },
-  { NOTE_E5, NOTE_C4, 16 },
-  { NOTE_D5,       0, 16 },
-  { NOTE_G4,       0, 16 },
-  { NOTE_D5,       0, 16 },
-  { NOTE_F5,       0, 16 },
-  { NOTE_E5,       0, 16 },
-  { NOTE_E5, NOTE_G4, 16 },
-  { NOTE_C5, NOTE_E4, 16 },
-  { NOTE_C5, NOTE_G4, 16 },
-  { NOTE_G5, NOTE_C4, 16 },
-  { NOTE_G5, NOTE_E4, 16 },
-  { NOTE_E5, NOTE_G3, 16 },
-  { NOTE_E5, NOTE_B3, 16 },
-  { NOTE_C6, NOTE_A3, 16 },
-  { NOTE_A5, NOTE_A3, 16 },
-  { NOTE_E5, NOTE_C4, 16 },
-  { NOTE_A5, NOTE_C4, 16 },
-  { NOTE_C5, NOTE_E4, 16 },
-  { NOTE_E5, NOTE_E4, 16 },
-  { NOTE_A4, NOTE_G4, 16 },
-  { NOTE_C5, NOTE_G4, 16 },
-  { NOTE_D5, NOTE_FS4, 16 },
-  { NOTE_D5, NOTE_A4, 16 },
-  { NOTE_FS5, NOTE_D4, 16 },
-  { NOTE_FS5, NOTE_FS4, 16 },
-  { NOTE_A5, NOTE_A3, 16 },
-  { NOTE_A5, NOTE_D4, 16 },
-  { NOTE_C6, NOTE_FS3, 16 },
-  { NOTE_C6, NOTE_A3, 16 },
-  { NOTE_B5, NOTE_G3, 16 },
-  { NOTE_G5, NOTE_G3, 16 },
-  { NOTE_D5, NOTE_B3, 16 },
-  { NOTE_G5, NOTE_B3, 16 },
-  { NOTE_B4, NOTE_D4, 16 },
-  { NOTE_D5, NOTE_D4, 16 },
-  { NOTE_G4, NOTE_FS4, 16 },
-  { NOTE_B4, NOTE_FS4, 16 },
-  { NOTE_C5, NOTE_E4, 16 },
-  { NOTE_C5, NOTE_G4, 16 },
-  { NOTE_E5, NOTE_C4, 16 },
-  { NOTE_E5, NOTE_E4, 16 },
-  { NOTE_G5, NOTE_G3, 16 },
-  { NOTE_G5, NOTE_C4, 16 },
-  { NOTE_B5, NOTE_E3, 16 },
-  { NOTE_B5, NOTE_G3, 16 },
-  { NOTE_A5, NOTE_FS3, 16 },
-  { NOTE_FS5, NOTE_FS3, 16 },
-  { NOTE_DS5, NOTE_A3, 16 },
-  { NOTE_FS5, NOTE_A3, 16 },
-  { NOTE_B4, NOTE_B3, 16 },
-  { NOTE_DS5, NOTE_B3, 16 },
-  { NOTE_FS4, NOTE_DS4, 16 },
-  { NOTE_A4, NOTE_DS4, 16 },
-  { NOTE_G4,       0, 16 },
-  { NOTE_G4, NOTE_E4, 16 },
-  { NOTE_G5, NOTE_C4, 16 },
-  { NOTE_G5, NOTE_E4, 16 },
-  { NOTE_G5, NOTE_A3, 16 },
-  { NOTE_E5, NOTE_C4, 16 },
-  { NOTE_C5, NOTE_E4, 16 },
-  { NOTE_E5, NOTE_G4, 16 },
-  { NOTE_A4, NOTE_FS4, 16 },
-  { NOTE_A4, NOTE_D4, 16 },
-  { NOTE_FS5, NOTE_B3, 16 },
-  { NOTE_FS5, NOTE_D4, 16 },
-  { NOTE_FS5, NOTE_G3, 16 },
-  { NOTE_D5, NOTE_B3, 16 },
-  { NOTE_B4, NOTE_D4, 16 },
-  { NOTE_D5, NOTE_FS4, 16 },
-  { NOTE_G4, NOTE_E4, 16 },
-  { NOTE_G4, NOTE_C4, 16 },
-  { NOTE_E5, NOTE_A3, 16 },
-  { NOTE_E5, NOTE_C4, 16 },
-  { NOTE_E5, NOTE_FS3, 16 },
-  { NOTE_C5, NOTE_A3, 16 },
-  { NOTE_A4, NOTE_C4, 16 },
-  { NOTE_C5, NOTE_C4, 16 },
-  { NOTE_FS4, NOTE_C4, 16 },
-  { NOTE_G5, NOTE_B3, 16 },
-  { NOTE_FS5, NOTE_C4, 16 },
-  { NOTE_E5, NOTE_A3, 16 },
-  { NOTE_DS5, NOTE_B3, 16 },
-  { NOTE_FS5, NOTE_B3, 16 },
-  { NOTE_B4, NOTE_B2, 16 },
-  { NOTE_DS5, NOTE_B2, 16 },
-  { NOTE_E5, NOTE_E3, 16 },
-  { NOTE_E5, NOTE_E4, 16 },
-  {       0, NOTE_B3, 16 },
-  {       0, NOTE_G3, 16 },
-  {       0, NOTE_E3, 16 },
-  {       0, NOTE_B2, 16 },
-  {       0, NOTE_G2, 16 },
-  {       0, NOTE_B2, 16 },
-  {       0, NOTE_E2, 4 },
-  { 0, 0, 4 },
-  { 0, 0, 2 },
-  { -1, -1, 0 }
+#ifdef USE_EFX
+#define EFX_GLISS B01000000
+#define EFX_VOICE1    B00000000
+#define EFX_VOICE2    B00100000
+#endif
+// uncomment for LED diagnostic
+//#define DIAG 0
+
+typedef struct Note {
+  byte effect;
+  int voice1;
+  int voice2;
+  byte duration;
+} Note;
+
+const Note score[] PROGMEM = {
+  { 0,       0, NOTE_E2, 16 },
+  { 0, NOTE_E4, NOTE_E2, 16 },
+  { 0, NOTE_A4, NOTE_A3, 16 },
+  { 0, NOTE_C5, NOTE_A3, 16 },
+  { 0, NOTE_B4, NOTE_A3, 16 },
+  { 0, NOTE_E4, NOTE_A3, 16 },
+  { 0, NOTE_B4, NOTE_GS3, 16 },
+  { 0, NOTE_D5, NOTE_GS3, 16 },
+  { 0, NOTE_C5, NOTE_A3, 16 },
+  { 0, NOTE_C5, NOTE_E3, 16 },
+  { 0, NOTE_E5, NOTE_A3, 16 },
+  { 0, NOTE_E5, NOTE_C4, 16 },
+  { 0, NOTE_GS4, NOTE_B3, 16 },
+  { 0, NOTE_GS4, NOTE_E3, 16 },
+  { 0, NOTE_E5, NOTE_B3, 16 },
+  { 0, NOTE_E5, NOTE_D4, 16 },
+  { 0, NOTE_A4, NOTE_C4, 16 },
+  { 0, NOTE_E4, NOTE_C4, 16 },
+  { 0, NOTE_A4, NOTE_A3, 16 },
+  { 0, NOTE_C5, NOTE_A3, 16 },
+  { 0, NOTE_B4, NOTE_GS3, 16 },
+  { 0, NOTE_E4, NOTE_GS3, 16 },
+  { 0, NOTE_B4, NOTE_E3, 16 },
+  { 0, NOTE_D5, NOTE_E3, 16 },
+  { 0, NOTE_C5, NOTE_A3, 16 },
+  { 0, NOTE_C5, NOTE_E3, 16 },
+  { 0, NOTE_A4, NOTE_A3, 16 },
+  { 0, NOTE_A4, NOTE_C4, 16 },
+  { 0,       0, NOTE_B3, 16 },
+  { 0,       0, NOTE_E3, 16 },
+  { 0,       0, NOTE_B3, 16 },
+  { 0,       0, NOTE_D4, 16 },
+  { 0,       0, NOTE_C4, 16 },
+  { 0, NOTE_E5, NOTE_C4, 16 },
+  { 0, NOTE_C5, NOTE_A3, 16 },
+  { 0, NOTE_E5, NOTE_A3, 16 },
+  { 0, NOTE_A4, NOTE_C4, 16 },
+  { 0, NOTE_C5, NOTE_C4, 16 },
+  { 0, NOTE_E4, NOTE_A3, 16 },
+  { 0, NOTE_G4, NOTE_A3, 16 },
+  { 0, NOTE_F4, NOTE_D4, 16 },
+  { 0, NOTE_F4, NOTE_A3, 16 },
+  { 0, NOTE_A4, NOTE_F3, 16 },
+  { 0, NOTE_A4, NOTE_A3, 16 },
+  { 0, NOTE_D5, NOTE_D3, 16 },
+  { 0, NOTE_D5, NOTE_F3, 16 },
+  { 0, NOTE_F5, NOTE_E2, 16 },
+  { 0, NOTE_F5, NOTE_C3, 16 },
+  { 0, NOTE_F5, NOTE_B2, 16 },
+  { 0, NOTE_D5, NOTE_B2, 16 },
+  { 0, NOTE_B4, NOTE_D3, 16 },
+  { 0, NOTE_D5, NOTE_D3, 16 },
+  { 0, NOTE_G4, NOTE_E3, 16 },
+  { 0, NOTE_B4, NOTE_E3, 16 },
+  { 0, NOTE_D4, NOTE_B3, 16 },
+  { 0, NOTE_F4, NOTE_B3, 16 },
+  { 0, NOTE_E4, NOTE_B3, 16 },
+  { 0, NOTE_E4, NOTE_E3, 16 },
+  { 0, NOTE_G4, NOTE_C3, 16 },
+  { 0, NOTE_G4, NOTE_E3, 16 },
+  { 0, NOTE_C5, NOTE_A2, 16 },
+  { 0, NOTE_C5, NOTE_C3, 16 },
+  { 0, NOTE_E5, NOTE_G2, 16 },
+  { 0, NOTE_E5, NOTE_B2, 16 },
+  { 0, NOTE_E5, NOTE_A2, 16 },
+  { 0, NOTE_C5, NOTE_A2, 16 },
+  { 0, NOTE_A4, NOTE_C3, 16 },
+  { 0, NOTE_C5, NOTE_C3, 16 },
+  { 0, NOTE_F4, NOTE_D3, 16 },
+  { 0, NOTE_F4, NOTE_F3, 16 },
+  { 0, NOTE_D5, NOTE_B2, 16 },
+  { 0, NOTE_D5, NOTE_D3, 16 },
+  { 0, NOTE_D5, NOTE_G2, 16 },
+  { 0, NOTE_B4, NOTE_G2, 16 },
+  { 0, NOTE_G4, NOTE_B2, 16 },
+  { 0, NOTE_B4, NOTE_B2, 16 },
+  { 0, NOTE_E4, NOTE_C3, 16 },
+  { 0, NOTE_E4, NOTE_E3, 16 },
+  { 0, NOTE_C5, NOTE_A2, 16 },
+  { 0, NOTE_C5, NOTE_C3, 16 },
+  { 0, NOTE_C5, NOTE_F2, 16 },
+  { 0, NOTE_A4, NOTE_F2, 16 },
+  { 0, NOTE_F4, NOTE_G2, 16 },
+  { 0, NOTE_A4, NOTE_G2, 16 },
+  { 0, NOTE_D4, NOTE_G2, 16 },
+  { 0, NOTE_D4, NOTE_G3, 16 },
+  { 0, NOTE_B4, NOTE_F3, 16 },
+  { 0, NOTE_B4, NOTE_G3, 16 },
+  { 0, NOTE_C5, NOTE_C3, 16 },
+  { 0, NOTE_C5, NOTE_G3, 16 },
+  { 0,       0, NOTE_C4, 16 },
+  { 0,       0, NOTE_E4, 16 },
+  { 0,       0, NOTE_D4, 16 },
+  { 0,       0, NOTE_G3, 16 },
+  { 0,       0, NOTE_D4, 16 },
+  { 0,       0, NOTE_F4, 16 },
+  { 0,       0, NOTE_E4, 16 },
+  { 0, NOTE_G4, NOTE_E4, 16 },
+  { 0, NOTE_C5, NOTE_C4, 16 },
+  { 0, NOTE_E5, NOTE_C4, 16 },
+  { 0, NOTE_D5, NOTE_B3, 16 },
+  { 0, NOTE_G4, NOTE_B3, 16 },
+  { 0, NOTE_D5, NOTE_G3, 16 },
+  { 0, NOTE_F5, NOTE_G3, 16 },
+  { 0, NOTE_E5, NOTE_C4, 16 },
+  { 0, NOTE_E5, NOTE_G3, 16 },
+  { 0, NOTE_G5, NOTE_C4, 16 },
+  { 0, NOTE_G5, NOTE_E4, 16 },
+  { 0, NOTE_B4, NOTE_D4, 16 },
+  { 0, NOTE_B4, NOTE_G3, 16 },
+  { 0, NOTE_G5, NOTE_D4, 16 },
+  { 0, NOTE_G5, NOTE_F4, 16 },
+  { 0, NOTE_C5, NOTE_E4, 16 },
+  { 0, NOTE_G4, NOTE_E4, 16 },
+  { 0, NOTE_C5, NOTE_C4, 16 },
+  { 0, NOTE_E5, NOTE_C4, 16 },
+  { 0, NOTE_D5,       0, 16 },
+  { 0, NOTE_G4,       0, 16 },
+  { 0, NOTE_D5,       0, 16 },
+  { 0, NOTE_F5,       0, 16 },
+  { 0, NOTE_E5,       0, 16 },
+  { 0, NOTE_E5, NOTE_G4, 16 },
+  { 0, NOTE_C5, NOTE_E4, 16 },
+  { 0, NOTE_C5, NOTE_G4, 16 },
+  { 0, NOTE_G5, NOTE_C4, 16 },
+  { 0, NOTE_G5, NOTE_E4, 16 },
+  { 0, NOTE_E5, NOTE_G3, 16 },
+  { 0, NOTE_E5, NOTE_B3, 16 },
+  { 0, NOTE_C6, NOTE_A3, 16 },
+  { 0, NOTE_A5, NOTE_A3, 16 },
+  { 0, NOTE_E5, NOTE_C4, 16 },
+  { 0, NOTE_A5, NOTE_C4, 16 },
+  { 0, NOTE_C5, NOTE_E4, 16 },
+  { 0, NOTE_E5, NOTE_E4, 16 },
+  { 0, NOTE_A4, NOTE_G4, 16 },
+  { 0, NOTE_C5, NOTE_G4, 16 },
+  { 0, NOTE_D5, NOTE_FS4, 16 },
+  { 0, NOTE_D5, NOTE_A4, 16 },
+  { 0, NOTE_FS5, NOTE_D4, 16 },
+  { 0, NOTE_FS5, NOTE_FS4, 16 },
+  { 0, NOTE_A5, NOTE_A3, 16 },
+  { 0, NOTE_A5, NOTE_D4, 16 },
+  { 0, NOTE_C6, NOTE_FS3, 16 },
+  { 0, NOTE_C6, NOTE_A3, 16 },
+  { 0, NOTE_B5, NOTE_G3, 16 },
+  { 0, NOTE_G5, NOTE_G3, 16 },
+  { 0, NOTE_D5, NOTE_B3, 16 },
+  { 0, NOTE_G5, NOTE_B3, 16 },
+  { 0, NOTE_B4, NOTE_D4, 16 },
+  { 0, NOTE_D5, NOTE_D4, 16 },
+  { 0, NOTE_G4, NOTE_FS4, 16 },
+  { 0, NOTE_B4, NOTE_FS4, 16 },
+  { 0, NOTE_C5, NOTE_E4, 16 },
+  { 0, NOTE_C5, NOTE_G4, 16 },
+  { 0, NOTE_E5, NOTE_C4, 16 },
+  { 0, NOTE_E5, NOTE_E4, 16 },
+  { 0, NOTE_G5, NOTE_G3, 16 },
+  { 0, NOTE_G5, NOTE_C4, 16 },
+  { 0, NOTE_B5, NOTE_E3, 16 },
+  { 0, NOTE_B5, NOTE_G3, 16 },
+  { 0, NOTE_A5, NOTE_FS3, 16 },
+  { 0, NOTE_FS5, NOTE_FS3, 16 },
+  { 0, NOTE_DS5, NOTE_A3, 16 },
+  { 0, NOTE_FS5, NOTE_A3, 16 },
+  { 0, NOTE_B4, NOTE_B3, 16 },
+  { 0, NOTE_DS5, NOTE_B3, 16 },
+  { 0, NOTE_FS4, NOTE_DS4, 16 },
+  { 0, NOTE_A4, NOTE_DS4, 16 },
+  { 0, NOTE_G4,       0, 16 },
+  { 0, NOTE_G4, NOTE_E4, 16 },
+  { 0, NOTE_G5, NOTE_C4, 16 },
+  { 0, NOTE_G5, NOTE_E4, 16 },
+  { 0, NOTE_G5, NOTE_A3, 16 },
+  { 0, NOTE_E5, NOTE_C4, 16 },
+  { 0, NOTE_C5, NOTE_E4, 16 },
+  { 0, NOTE_E5, NOTE_G4, 16 },
+  { 0, NOTE_A4, NOTE_FS4, 16 },
+  { 0, NOTE_A4, NOTE_D4, 16 },
+  { 0, NOTE_FS5, NOTE_B3, 16 },
+  { 0, NOTE_FS5, NOTE_D4, 16 },
+  { 0, NOTE_FS5, NOTE_G3, 16 },
+  { 0, NOTE_D5, NOTE_B3, 16 },
+  { 0, NOTE_B4, NOTE_D4, 16 },
+  { 0, NOTE_D5, NOTE_FS4, 16 },
+  { 0, NOTE_G4, NOTE_E4, 16 },
+  { 0, NOTE_G4, NOTE_C4, 16 },
+  { 0, NOTE_E5, NOTE_A3, 16 },
+  { 0, NOTE_E5, NOTE_C4, 16 },
+  { 0, NOTE_E5, NOTE_FS3, 16 },
+  { 0, NOTE_C5, NOTE_A3, 16 },
+  { 0, NOTE_A4, NOTE_C4, 16 },
+  { 0, NOTE_C5, NOTE_C4, 16 },
+  { 0, NOTE_FS4, NOTE_C4, 16 },
+  { 0, NOTE_G5, NOTE_B3, 16 },
+  { 0, NOTE_FS5, NOTE_C4, 16 },
+  { 0, NOTE_E5, NOTE_A3, 16 },
+  { 0, NOTE_DS5, NOTE_B3, 16 },
+  { 0, NOTE_FS5, NOTE_B3, 16 },
+  { 0, NOTE_B4, NOTE_B2, 16 },
+  { 0, NOTE_DS5, NOTE_B2, 16 },
+  { 0, NOTE_E5, NOTE_E3, 16 },
+  { 0, NOTE_E5, NOTE_E4, 16 },
+  { 0,       0, NOTE_B3, 16 },
+  { 0,       0, NOTE_G3, 16 },
+  { 0,       0, NOTE_E3, 16 },
+  { 0,       0, NOTE_B2, 16 },
+  { 0,       0, NOTE_G2, 16 },
+  { 0,       0, NOTE_B2, 16 },
+  { 0,       0, NOTE_E2, 4 },
+  { 0, 0, 0, 4 },
+  { 0, 0, 0, 2 },
+  { 0, -1, -1, 0 }
 };
 
-int t[] = { 0, 0, 0 };
+int t[] = { 0, 0 };
+int tp[] = { 0, 0 };
 
+#ifdef USE_EFX
+boolean doSound(byte efx, int r) {
+#else
 boolean doSound(int r) {
+#endif
   static byte voice = 0;
   static int voice_period = 0;
   static int pulse = 0;
+  int f;
   
   if (!t[voice]) {
     PORTB &= B11111001;
@@ -246,19 +267,28 @@ boolean doSound(int r) {
     pulse = 0;
     voice = !voice;
   }
-  if (t[voice]) {
+#ifdef USE_EFX
+  boolean g = (efx & EFX_GLISS) && (efx & (voice << 5) == voice ) && (tp[voice] != t[voice]);
+  f =  g ? tp[voice] : t[voice];
+#else
+  f = t[voice];
+#endif
+  if (f) {
     if (voice_period == 0) {
       PORTB &= B11111101;
       PORTB |= B00000100;
-    } else if(voice_period == (PERIOD / 2) / t[voice]) {
+    } else if(voice_period == (PERIOD / 2) / f) {
       PORTB &= B11111011;
       PORTB |= B00000010;
     }
   }
-  delayMicroseconds(DELAY_MILLIS);
-  voice_period = (voice_period + 1 < PERIOD / t[voice]) ? voice_period + 1 : 0;
+  delayMicroseconds(DELAY_MICROS);
+  voice_period = (voice_period + 1 < PERIOD / f) ? voice_period + 1 : 0;
   if (voice_period == 0) {
-    if (++pulse > t[voice] / r) {
+#ifdef USE_EFX
+    if(g) tp[voice] = tp[voice] != t[voice] ? (tp[voice] < t[voice] ? min(t[voice], tp[voice] + (efx & B00111111)) : max(t[voice], tp[voice] - (efx & B00111111))) : t[voice];
+#endif
+    if (++pulse > f / r) {
       pulse = 0;
       voice = !voice;
     }
@@ -268,7 +298,7 @@ boolean doSound(int r) {
 void setup() {
   DDRB |= B00000110;
 #ifdef DIAG
-  pinMode(0, OUTPUT);
+  pinMode(DIAG, OUTPUT);
 #endif
 }
 
@@ -277,20 +307,32 @@ void loop() {
   boolean lit = false;
 #endif
   int i = 0, r = 16;
+#ifdef USE_EFX
+  byte effect;
+#endif
+  byte duration;
   while(++i) {
     r = analogRead(A3) / 4;
     r = r < 16 ? 16 : r;
-    t[0] = pgm_read_word_near(&score[i][0]);
+    tp[0] = t[0]; tp[1] = t[1];
+    t[0] = pgm_read_word(&score[i].voice1);
     if(t[0] < 0) {
-      i = 0;
-      t[0] = pgm_read_word_near(&score[i][0]);
+      break;
     }
-    t[1] = pgm_read_word_near(&score[i][1]);
-#ifdef DIAG
-    digitalWrite(0, lit = !lit);
+#ifdef USE_EFX
+    effect = pgm_read_byte(&score[i].effect);
 #endif
-    for(int j = BEAT/(pgm_read_word_near(&score[i][2])); j; j--) {
+    t[1] = pgm_read_word(&score[i].voice2);
+    duration = pgm_read_byte(&score[i].duration);
+#ifdef DIAG
+    digitalWrite(DIAG, lit = !lit);
+#endif
+    for(int j = BEAT/duration; j; j--) {
+#ifdef USE_EFX
+      doSound(effect, r);
+#else
       doSound(r);
+#endif
     }
   }
 }
